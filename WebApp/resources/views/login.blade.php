c30759f231a25441bdfe7bc7ef14483eb6d59e37
Jakub Bas <71661812+jakubbas@users.noreply.github.com>
2024-05-06 13:46:39 +0000

    hdfhfd

---
+++ WebApp/resources/views/login.blade.php
@@ -0,0 +1,39 @@
+@extends('layout')
+
+@section('default')
+
+
+<head>
+    <link href="/css/login.css" rel="stylesheet">
+</head>
+
+<body>
+
+
+    <div class="login-container">
+        <h2>Login</h2>
+        <form action="" method="POST">
+            @csrf <!-- CSRF protection for Laravel -->
+            <div class="form-group">
+                <label for="username">Username:</label>
+                <input type="text" id="username" name="username" required>
+            </div>
+            <div class="form-group">
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" required>
+            </div>
+            <button type="submit">Login</button>
+        </form>
+    </div>
+
+</body>
+
+
+
+
+
+
+
+
+
+@endsection
--- WebApp/routes/web.php
+++ WebApp/routes/web.php
@@ -44,6 +44,10 @@ Route::get('/home', function () {
     return view('home');
 }); 
 
+Route::get('/login', function () {
+    return view('login');
+}); 
+
 Route::get('/swimmers', function () {
     return view('/swimmers');
 }); 
